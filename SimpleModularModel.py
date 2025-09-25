f3fdf15638a4d67a47e0df07a9c86a44e901a68c
Reno50 <59569424+Reno50@users.noreply.github.com>
2025-09-25 13:38:13 -0400

    Added the command to run the docker back

--- SimpleModularModel.py
+++ SimpleModularModel.py
@@ -2,6 +2,10 @@ from Solvers.BasicChamberSolver import create_enhanced_solver
 from physicsnemo.sym.hydra import instantiate_arch, PhysicsNeMoConfig
 import physicsnemo.sym
 
+# Run with docker run --gpus all --shm-size=1g --ulimit memlock=-1 --ulimit stack=67108864 --runtime nvidia --rm -v ./:/workspace -it nvcr.io/nvidia/physicsnemo/physicsnemo:25.06 bash
+
+# Then do python SimpleModularModel.py
+
 @physicsnemo.sym.main(config_path="conf", config_name="config")
 def main(cfg: PhysicsNeMoConfig):
     create_enhanced_solver(cfg)
