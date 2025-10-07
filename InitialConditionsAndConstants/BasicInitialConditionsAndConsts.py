Fixed chamber size normalization

InitialConditionsAndConstants/BasicInitialConditionsAndConsts.py
@@ -4,6 +4,7 @@ def generate_initial_temps(x, y) -> list[int]: # A list of temperatures at each 
     '''
     Given two nparrays, return the initial temp - pretty simple, going off the diagram on page 171 of the paper
     '''
+    chamber_width, chamber_height = 20000, 6000 # Same for chamber size - normalize to 0 - 1
     # Now they are one dimensional arrays
     x_flat = np.array(x).flatten() # a number of Xs
     y_flat = np.array(y).flatten() # an identical number of Ys
@@ -11,9 +12,9 @@ def generate_initial_temps(x, y) -> list[int]: # A list of temperatures at each 
     returnVals = [0 for i in range(len(x_flat))] # an identical number of 0s for each point
 
     for i in range(len(returnVals)):
-        if ((x_flat[i] > 6000) or (y_flat[i] > 3000)):
-            returnVals[i] = 20.0 + 25.0 * (y_flat[i] / 1000.0)
+        if (x_flat[i] > (6000/chamber_width)) or (y_flat[i] > (3000/chamber_height)):
+            returnVals[i] = 20.0 + 25.0 * (y_flat[i] / (1000.0/chamber_height))
         else:
-            returnVals[i] = 900 - (700 * y_flat[i] / 6000) # pluton is 900 degrees celcius, we want a smooth transition though
+            returnVals[i] = 900 - (700 * y_flat[i] / (6000/chamber_height)) # pluton is 900 degrees celcius, we want a smooth transition though
     
     return returnVals