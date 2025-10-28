8c10d9125780a248594e6f62e095c65ed69c2053
Reno50 <59569424+Reno50@users.noreply.github.com>
2025-10-28 14:44:47 -0400

    Changed the initial conditions to be smoother

--- InitialConditionsAndConstants/BasicInitialConditionsAndConsts.py
+++ InitialConditionsAndConstants/BasicInitialConditionsAndConsts.py
@@ -14,7 +14,15 @@ def generate_initial_temps(x, y) -> list[int]: # A list of temperatures at each 
     for i in range(len(returnVals)):
         if (x_flat[i] > (6000/chamber_width)) or (y_flat[i] > (3000/chamber_height)):
             returnVals[i] = 170.0 - 25.0 * (y_flat[i] / (1000.0/chamber_height))
+        elif (x_flat[i] > (5000/chamber_width)) or (y_flat[i] > (2400/chamber_height)): # Smooth the boundary
+            # The x value - 0 meaning at 5000, 1 meaning 6000
+            # The y value - same thing ish
+            x = ((x_flat[i] - (5000/chamber_width)) * chamber_width) / 1000
+            y = ((y_flat[i] - (2400/chamber_height)) * chamber_height) / 600
+            num = max(x, y)
+            returnVals[i] = num * 170.0 - 25.0 * (y_flat[i] / (1000.0/chamber_height)) + (1-num) * 900
+            # Basically average the two
         else:
-            returnVals[i] = 900 - (700 * y_flat[i]) - (700 * x_flat[i])
+            returnVals[i] = 900
     
     return returnVals
